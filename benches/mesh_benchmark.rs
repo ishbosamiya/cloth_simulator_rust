c6d4c997f46669ca9da98809b065b4eb40eb184b
ishbosamiya <ishbosamiya@gmail.com>
2021-02-05 11:36:34 +0530

    benchmark: mesh: generate_gl_mesh()

---
+++ benches/mesh_benchmark.rs
@@ -0,0 +1,32 @@
+use cloth_simulator_rust::mesh::Mesh;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+pub fn mesh_generate_gl_mesh_benchmark(c: &mut Criterion) {
+    let glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+    let (mut window, _) = glfw
+        .create_window(
+            1280,
+            720,
+            "Mesh Generate GL Mesh Benchmark",
+            glfw::WindowMode::Windowed,
+        )
+        .expect("Failed to create glfw window");
+    gl::load_with(|symbol| window.get_proc_address(symbol));
+
+    let use_face_normal = false;
+    let mut mesh = Mesh::new();
+    mesh.read(&std::path::Path::new("models/monkey_subd_03.obj"))
+        .unwrap();
+    c.bench_with_input(
+        criterion::BenchmarkId::new("mesh_generate_gl_mesh", use_face_normal),
+        &use_face_normal,
+        |b, use_face_normal| {
+            b.iter(|| {
+                mesh.generate_gl_mesh(black_box(*use_face_normal));
+            })
+        },
+    );
+}
+
+criterion_group!(benches, mesh_generate_gl_mesh_benchmark);
+criterion_main!(benches);
