4b600387c1bfed7e76664bb305aa09eb5945630d
ishbosamiya <ishbosamiya@gmail.com>
2021-03-24 00:25:06 +0530

    bvh: balance()

--- src/bvh.rs
+++ src/bvh.rs
@@ -25,6 +25,7 @@ lazy_static! {
 
 type Scalar = f64;
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 struct BVHNodeIndex(pub Index);
 
 struct BVHNode<T> {
@@ -112,8 +113,73 @@ pub struct BVHTree<T> {
     totbranch: usize,
     start_axis: u8,
     stop_axis: u8,
-    axis: u8,      // kdop type (6 => OBB, 8 => AABB, etc.)
+    _axis: u8,     // kdop type (6 => OBB, 8 => AABB, etc.)
     tree_type: u8, // Type of tree (4 => QuadTree, etc.)
+}
+
+struct BVHBuildHelper {
+    totleafs: usize,
+    leafs_per_child: [usize; 32], // Min number of leafs that are archievable from a node at depth N
+    branches_on_level: [usize; 32], // Number of nodes at depth N (tree_type^N)
+    remain_leafs: usize, // Number of leafs that are placed on the level that is not 100% filled
+}
+
+impl BVHBuildHelper {
+    fn new(
+        totleafs: usize,
+        leafs_per_child: [usize; 32],
+        branches_on_level: [usize; 32],
+        remain_leafs: usize,
+    ) -> Self {
+        return Self {
+            totleafs,
+            leafs_per_child,
+            branches_on_level,
+            remain_leafs,
+        };
+    }
+
+    /// Return the min index of all the leafs achievable with the given branch
+    fn implicit_leafs_index(&self, depth: usize, child_index: usize) -> usize {
+        let min_leaf_index = child_index * self.leafs_per_child[depth - 1];
+        if min_leaf_index <= self.remain_leafs {
+            return min_leaf_index;
+        } else if self.leafs_per_child[depth] != 0 {
+            return self.totleafs
+                - (self.branches_on_level[depth - 1] - child_index) * self.leafs_per_child[depth];
+        } else {
+            return self.remain_leafs;
+        }
+    }
+}
+
+struct BVHDivNodesData<'a> {
+    brances_array_start: usize,
+    tree_offset: usize,
+    data: &'a BVHBuildHelper,
+    depth: usize,
+    i: usize,
+    first_of_next_level: usize,
+}
+
+impl<'a> BVHDivNodesData<'a> {
+    fn new(
+        brances_array_start: usize,
+        tree_offset: usize,
+        data: &'a BVHBuildHelper,
+        depth: usize,
+        i: usize,
+        first_of_next_level: usize,
+    ) -> Self {
+        return Self {
+            brances_array_start,
+            tree_offset,
+            data,
+            depth,
+            i,
+            first_of_next_level,
+        };
+    }
 }
 
 impl<T> BVHTree<T> {
@@ -174,7 +240,7 @@ impl<T> BVHTree<T> {
             totbranch: 0,
             start_axis,
             stop_axis,
-            axis,
+            _axis: axis,
             tree_type,
         };
     }
@@ -195,6 +261,312 @@ impl<T> BVHTree<T> {
             let axis_iter = axis_iter as usize;
             node.bv[(2 * axis_iter)] -= self.epsilon; // min
             node.bv[(2 * axis_iter) + 1] += self.epsilon; // max
+        }
+    }
+
+    fn refit_kdop_hull(&mut self, node_index: BVHNodeIndex, start: usize, end: usize) {
+        {
+            let node = self.node_array.get_mut(node_index.0).unwrap();
+            node.min_max_init(self.start_axis, self.stop_axis);
+        }
+
+        for j in start..end {
+            let (node, node_2) = self.node_array.get2_mut(node_index.0, self.nodes[j].0);
+            let node = node.unwrap();
+            let bv = &mut node.bv;
+            let node_bv = &mut node_2.unwrap().bv;
+
+            for axis_iter in self.start_axis..self.stop_axis {
+                let axis_iter = axis_iter as usize;
+
+                let new_min = node_bv[(2 * axis_iter)];
+                if new_min < bv[(2 * axis_iter)] {
+                    bv[(2 * axis_iter)] = new_min;
+                }
+
+                let new_max = node_bv[(2 * axis_iter) + 1];
+                if new_max > bv[(2 * axis_iter) + 1] {
+                    bv[(2 * axis_iter) + 1] = new_max;
+                }
+            }
+        }
+    }
+
+    fn build_implicit_helper(&self) -> BVHBuildHelper {
+        let totleafs = self.totleaf;
+        let tree_type = self.tree_type as usize;
+
+        // calculate smallest tree_type^n such that tree_type^n >= self.num_leafs
+        let mut leafs_per_child: [usize; 32] = [0; 32];
+        leafs_per_child[0] = 1;
+        while leafs_per_child[0] < totleafs {
+            leafs_per_child[0] *= tree_type;
+        }
+
+        let mut branches_on_level: [usize; 32] = [0; 32];
+        let mut depth = 1;
+        branches_on_level[0] = 1;
+        while depth < 32 && (leafs_per_child[depth - 1] != 0) {
+            branches_on_level[depth] = branches_on_level[depth - 1] * tree_type;
+            leafs_per_child[depth] = leafs_per_child[depth - 1] / tree_type;
+            depth += 1;
+        }
+
+        let remain = totleafs - leafs_per_child[1];
+        let nnodes = (remain + tree_type - 2) / (tree_type - 1);
+        let remain_leafs = remain + nnodes;
+
+        return BVHBuildHelper::new(totleafs, leafs_per_child, branches_on_level, remain_leafs);
+    }
+
+    fn bvh_insertion_sort(&mut self, lo: usize, hi: usize, axis: usize) {
+        for i in lo..hi {
+            let mut j = i;
+            let node_t_index = self.nodes[i];
+            let node_t = self.node_array.get(node_t_index.0).unwrap();
+            let mut node_j_minus_one = self.node_array.get(self.nodes[j - 1].0).unwrap();
+            while (j != lo) && (node_t.bv[axis] < node_j_minus_one.bv[axis]) {
+                self.nodes[j] = self.nodes[j - 1];
+                j -= 1;
+                node_j_minus_one = self.node_array.get(self.nodes[j - 1].0).unwrap();
+            }
+            self.nodes[j] = node_t_index;
+        }
+    }
+
+    fn bvh_partition(
+        &mut self,
+        lo: usize,
+        hi: usize,
+        node_x_index: BVHNodeIndex,
+        axis: usize,
+    ) -> usize {
+        let mut i = lo;
+        let mut j = hi;
+        let node_x = self.node_array.get(node_x_index.0).unwrap();
+        loop {
+            let mut node_a_i = self.node_array.get(self.nodes[i].0).unwrap();
+            while node_a_i.bv[axis] < node_x.bv[axis] {
+                i += 1;
+                node_a_i = self.node_array.get(self.nodes[i].0).unwrap();
+            }
+
+            let mut node_a_j = self.node_array.get(self.nodes[j].0).unwrap();
+            while node_x.bv[axis] < node_a_j.bv[axis] {
+                j -= 1;
+                node_a_j = self.node_array.get(self.nodes[j].0).unwrap();
+            }
+
+            if !(i < j) {
+                return i;
+            }
+
+            let temp = self.nodes[i];
+            self.nodes[i] = self.nodes[j];
+            self.nodes[j] = temp;
+
+            i += 1;
+        }
+    }
+
+    fn bvh_median_of_3(&self, lo: usize, mid: usize, hi: usize, axis: usize) -> BVHNodeIndex {
+        let node_lo = self.node_array.get(self.nodes[lo].0).unwrap();
+        let node_mid = self.node_array.get(self.nodes[mid].0).unwrap();
+        let node_hi = self.node_array.get(self.nodes[hi].0).unwrap();
+
+        if node_mid.bv[axis] < node_lo.bv[axis] {
+            if node_hi.bv[axis] < node_mid.bv[axis] {
+                return self.nodes[mid];
+            } else {
+                if node_hi.bv[axis] < node_lo.bv[axis] {
+                    return self.nodes[hi];
+                } else {
+                    return self.nodes[lo];
+                }
+            }
+        } else {
+            if node_hi.bv[axis] < node_mid.bv[axis] {
+                if node_hi.bv[axis] < node_lo.bv[axis] {
+                    return self.nodes[lo];
+                } else {
+                    return self.nodes[hi];
+                }
+            } else {
+                return self.nodes[mid];
+            }
+        }
+    }
+
+    fn partition_nth_element(&mut self, mut begin: usize, mut end: usize, n: usize, axis: usize) {
+        while (end - begin) > 3 {
+            let cut = self.bvh_partition(
+                begin,
+                end,
+                self.bvh_median_of_3(begin, (begin + end) / 2, end - 1, axis),
+                axis,
+            );
+
+            if cut <= n {
+                begin = cut;
+            } else {
+                end = cut;
+            }
+        }
+
+        self.bvh_insertion_sort(begin, end, axis);
+    }
+
+    fn split_leafs(&mut self, nth: &[usize], partitions: usize, split_axis: usize) {
+        for i in 0..(partitions - 1) {
+            if nth[i] >= nth[partitions] {
+                break;
+            }
+
+            self.partition_nth_element(nth[i], nth[partitions], nth[i + 1], split_axis);
+        }
+    }
+
+    fn non_recursive_bvh_div_nodes_task_cb(&mut self, data: &BVHDivNodesData, j: usize) {
+        let parent_level_index = j - data.i;
+
+        let mut nth_positions: [usize; (MAX_TREETYPE + 1) as usize] =
+            [0; (MAX_TREETYPE + 1) as usize];
+
+        let parent_leafs_begin = data
+            .data
+            .implicit_leafs_index(data.depth, parent_level_index);
+        let parent_leafs_end = data
+            .data
+            .implicit_leafs_index(data.depth, parent_level_index + 1);
+
+        let parent_index = BVHNodeIndex(
+            self.node_array
+                .get_unknown_index(data.brances_array_start + j),
+        );
+
+        // calculate the bounding box of this branch and chooses the
+        // longest axis as the axis to divide the leaves
+        self.refit_kdop_hull(parent_index, parent_leafs_begin, parent_leafs_end);
+        let parent = self.node_array.get_mut(parent_index.0).unwrap();
+        let split_axis = get_largest_axis(&parent.bv);
+
+        // Save split axis (this can be used on raytracing to speedup the query time)
+        parent.main_axis = split_axis / 2;
+
+        // Split the childs along the split_axis, note: its not needed
+        // to sort the whole leafs array.
+        // Only to assure that the elements are partitioned on a way
+        // that each child takes the elements it would take in case
+        // the whole array was sorted.
+        // Split_leafs takes care of that "sort" problem.
+        nth_positions[0] = parent_leafs_begin;
+        nth_positions[self.tree_type as usize] = parent_leafs_end;
+        for k in 1..self.tree_type {
+            let k = k as usize;
+            let child_index = j * self.tree_type as usize + data.tree_offset + k;
+            let child_level_index = child_index - data.first_of_next_level;
+            nth_positions[k] = data
+                .data
+                .implicit_leafs_index(data.depth + 1, child_level_index);
+        }
+
+        self.split_leafs(&nth_positions, self.tree_type.into(), split_axis.into());
+
+        // setup children and totnode counters
+        for k in 0..self.tree_type {
+            let k = k as usize;
+            let child_index = j * self.tree_type as usize + data.tree_offset + k;
+            let child_level_index = child_index - data.first_of_next_level;
+
+            let child_leafs_begin = data
+                .data
+                .implicit_leafs_index(data.depth + 1, child_level_index);
+            let child_leafs_end = data
+                .data
+                .implicit_leafs_index(data.depth + 1, child_level_index + 1);
+
+            if child_leafs_end - child_leafs_begin > 1 {
+                let child_index = BVHNodeIndex(
+                    self.node_array
+                        .get_unknown_index(data.brances_array_start + child_index),
+                );
+                let parent = self.node_array.get_mut(parent_index.0).unwrap();
+                parent.children[k] = child_index;
+                let child = self.node_array.get_mut(child_index.0).unwrap();
+                child.parent = Some(parent_index);
+            } else if child_leafs_end - child_leafs_begin == 1 {
+                let child_index = self.nodes[child_leafs_begin];
+                let parent = self.node_array.get_mut(parent_index.0).unwrap();
+                parent.children[k] = child_index;
+                let child = self.node_array.get_mut(child_index.0).unwrap();
+                child.parent = Some(parent_index);
+            } else {
+                break;
+            }
+        }
+
+        // TODO(ish):
+    }
+
+    fn non_recursive_bvh_div_nodes(&mut self, branches_array_start: usize, num_leafs: usize) {
+        let tree_type = self.tree_type;
+        let tree_offset = 2 - tree_type;
+        let num_branches = implicit_needed_branches(tree_type, num_leafs);
+
+        if num_leafs == 1 {
+            let root_index =
+                BVHNodeIndex(self.node_array.get_unknown_index(branches_array_start + 1)); // TODO(ish): verify this
+            self.refit_kdop_hull(root_index, 0, num_leafs);
+
+            let root = self.node_array.get_mut(root_index.0).unwrap();
+            root.main_axis = get_largest_axis(&root.bv) / 2;
+            root.totnode = 1;
+            root.children[0] = self.nodes[0];
+            let root_child_index = root.children[0];
+            let child = self.node_array.get_mut(root_child_index.0).unwrap();
+            child.parent = Some(root_index);
+            return;
+        }
+
+        let data = self.build_implicit_helper();
+
+        let mut cb_data =
+            BVHDivNodesData::new(branches_array_start, tree_offset.into(), &data, 0, 0, 0);
+
+        // loop tree levels, (log N) loops
+        let mut i = 1;
+        let mut depth = 1;
+        while i <= num_branches {
+            let first_of_next_level: usize = i * tree_type as usize + tree_offset as usize;
+            // index of last branch on this level
+            let i_stop = first_of_next_level.min(num_branches + 1);
+
+            // Loop all branches on this level
+            cb_data.first_of_next_level = first_of_next_level;
+            cb_data.i = i;
+            cb_data.depth = depth;
+
+            // TODO(ish): make this parallel, refer to Blender's code
+            for i_task in i..i_stop {
+                self.non_recursive_bvh_div_nodes_task_cb(&cb_data, i_task);
+            }
+
+            i = first_of_next_level;
+            depth += 1;
+        }
+    }
+
+    /// Call balance() after inserting the nodes using insert()
+    /// This function should be called only once
+    pub fn balance(&mut self) {
+        assert_eq!(self.totbranch, 0);
+
+        self.non_recursive_bvh_div_nodes(self.totleaf - 1, self.totleaf);
+
+        self.totbranch = implicit_needed_branches(self.tree_type, self.totleaf);
+        for i in 0..self.totbranch {
+            self.nodes[self.totleaf + i] =
+                BVHNodeIndex(self.node_array.get_unknown_index(self.totleaf + i));
         }
     }
 
@@ -234,6 +606,26 @@ impl<T> BVHTree<T> {
 
 fn implicit_needed_branches(tree_type: u8, leafs: usize) -> usize {
     return 1.max(leafs + tree_type as usize - 3) / (tree_type - 1) as usize;
+}
+
+fn get_largest_axis(bv: &Vec<Scalar>) -> u8 {
+    let middle_point_x = bv[1] - bv[0]; // x axis
+    let middle_point_y = bv[3] - bv[2]; // y axis
+    let middle_point_z = bv[5] - bv[4]; // z axis
+
+    if middle_point_x > middle_point_y {
+        if middle_point_x > middle_point_z {
+            return 1; // max x axis
+        } else {
+            return 5; // max z axis
+        }
+    } else {
+        if middle_point_y > middle_point_z {
+            return 3; // max y axis
+        } else {
+            return 5; // max z axis
+        }
+    }
 }
 
 trait ArenaFunctions {
